478babb32808f70b38f0e0f610df36360c281e71
wanchopen <wanchopen@gmail.com>
2017-01-19 16:50:59 +0300

    Added drawer for left column

--- src/components/LeftColumn/LeftColumn.js
+++ src/components/LeftColumn/LeftColumn.js
@@ -1,16 +1,39 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './LeftColumn.css';
+import Drawer from 'material-ui/Drawer';
+import IconButton from 'material-ui/IconButton';
+import MenuNavigation from 'material-ui/svg-icons/navigation/menu';
 import ProfileInfo from '../ProfileInfo';
 import LeftNavigation from '../LeftNavigation';
 
-function LeftColumn () {
-    return (
-        <div className={s.root}>
-            <ProfileInfo/>
-            <LeftNavigation/>
-        </div>
-    );
+class LeftColumn extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+    handleToggle = () => this.setState({open: !this.state.open});
+    handleClose = () => this.setState({open: false});
+
+    render () {
+        return (
+            <div className={s.root}>
+                <IconButton onTouchTap={this.handleToggle}>
+                    <MenuNavigation />
+                </IconButton>
+                <Drawer
+                    open={this.state.open}
+                    docked={false}
+                    onRequestChange={(open) => this.setState({open})}
+                >
+                    <ProfileInfo/>
+                    <LeftNavigation/>
+                </Drawer>
+            </div>
+        );
+    }
 }
 
 export default withStyles(s)(LeftColumn);
